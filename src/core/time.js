fbc9d5807d3a0f3035ea65cf41f888bd3d60f1e6
Samlv9 <Samlv9@live.com>
2017-04-18 17:44:44 +0800

    获取当前时间戳。

---
+++ src/core/time.js
@@ -0,0 +1,68 @@
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2017 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 1.0.0 | http://apidev.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+///                                              }|
+///                                              }|
+///                                              }|     　 へ　　　 ／|    
+///      _______     _______         ______      }|      /　│　　 ／ ／
+///     /  ___  |   |_   __ \      .' ____ '.    }|     │　Z ＿,＜　／　　 /`ヽ
+///    |  (__ \_|     | |__) |     | (____) |    }|     │　　　　　ヽ　　 /　　〉
+///     '.___`-.      |  __ /      '_.____. |    }|      Y　　　　　`　 /　　/
+///    |`\____) |    _| |  \ \_    | \____| |    }|    ｲ●　､　●　　⊂⊃〈　　/
+///    |_______.'   |____| |___|    \______,'    }|    ()　 v　　　　|　＼〈
+///    |=========================================\|    　>ｰ ､_　 ィ　 │ ／／
+///    |> LESS IS MORE                           ||     / へ　　 /　ﾉ＜|＼＼
+///    `=========================================/|    ヽ_ﾉ　　(_／　 │／／
+///                                              }|     7　　　　　　  |／
+///                                              }|     ＞―r￣￣`ｰ―＿`
+///                                              }|
+///                                              }|
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+/// 获取系统的当前时间戳。
+export const __TIME_CURR__ = (function () {
+    /// 优先使用高精度的时间。
+    if ( typeof performance != "undefined" ) {
+        if ( typeof performance.now == "function" ) { return () => performance.now(); }
+        if ( typeof performance.webkitNow == "function" ) { return () => performance.webkitNow(); }
+    }
+    
+    return () => Date.now();
+}());
+
+/// 定义初始时间。
+export const __TIME_INIT__ = __TIME_CURR__();
+
+/// 定义当前时区相对于 UTC 时区之间的时间差（以毫秒为单位）。
+export const __TIME_ZONE__ = ((new Date()).getTimezoneOffset() * -60000);
+
+/**
+ * 获取从初始时间至系统当前时间所经过的毫秒数（小数部分表示高精度时间）。
+ * @returns {Number}
+ */
+export default function time() {
+    let __C_TIME__ = __TIME_CURR__();
+    
+    if ( __C_TIME__ < __TIME_INIT__ ) { // 当前系统时间被外部非法修改。
+        __TIME_INIT__ = __C_TIME__;
+    }
+    
+    return (__C_TIME__ - __TIME_INIT__);
+}
